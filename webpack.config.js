29715ca7b9e8f007cd15b2322bf3f1b9b0a85724
evgeniylarkin <evglark@gmail.com>
2019-01-21 23:34:32 +0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'main.js',
-        publicPath: 'dist/'
+        publicPath: '/'
     },
     devServer: {
         overlay: true
     },
+    plugins: [
+        new CleanWebpackPlugin(['./dist']),
+        new HtmlWebpackPlugin({ template: './public/index.html', inject: false }),
+    ],
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                use: ['babel-loader', 'eslint-loader']
-            },
+                use: [
+                    'babel-loader',
+                    'eslint-loader'
+                ]
+            }
         ]
     }
 }
 
 module.exports = (env, options) => {
     let production = options.mode === 'production'
-    config.devtool = production ? false : 'source-map' 
+    config.devtool = production ? false : 'source-map'
 
     return config;
 }
